28c1264381368c2a0f804665ad762bf89d145425
changwanling <wcha305@aucklanduni.ac.nz>
2023-10-05 11:14:33 +1300

    q01

--- question-01/question-01.js
+++ question-01/question-01.js
@@ -29,9 +29,67 @@ app.get("/", function (req, res) {
 
 // YOU CAN CREATE YOU HANDLER FUNCTION HERE
 
+app.post("/submit", function(req, res) {
+  res.locals.name = req.body.name;
+  res.locals.languages = makeArray(req.body.language);
+  res.locals.interests = makeArray(req.body.interest);
+
+  const spend = calculateTimeSpending(req.body);
+  res.locals.writing = spend.writing;
+  res.locals.debugging = spend.debugging;
+  res.locals.stackoverflow = spend.stackoverflow;
+  console.log(generateTableData(req.body));
+  res.locals.goals = generateTableData(req.body);
+
+  res.render("survey");
+});
+
 // Start the server running. Once the server is running, the given function will be called, which will
 // log a simple message to the server console. Any console.log() statements in your node.js code
 // can be seen in the terminal window used to run the server.
 app.listen(port, function () {
   console.log(`Example app listening on port ${port}!`);
 });
+
+function makeArray(input) {
+  if (input === undefined) {
+      return [];
+  }
+  else if (Array.isArray(input)) {
+      return input;
+  }
+  else {
+      return [input];
+  }
+}
+
+function calculateTimeSpending(data){
+  const writing = parseFloat(data.writing);
+  const debugging = parseFloat(data.debugging);
+  const stackOverflow = parseFloat(data.stackoverflow);
+  const totalTime = writing + debugging + stackOverflow;
+
+  const spend = {
+    writing : ((writing / totalTime) * 100).toFixed(2),
+    debugging : ((debugging / totalTime) * 100).toFixed(2),
+    stackoverflow : ((stackOverflow / totalTime) * 100).toFixed(2)
+  }
+
+  return spend;
+}
+
+function generateTableData(data) {
+  const goals = [
+  ];
+  for(let i=0; i < data.goals.length; i++ ){
+    if(!data.goals[i] &&  !data.goalsTime[i]){
+      continue;
+    }
+    const goal = {
+      goal : data.goals[i],
+      time: data.goalsTime[i]
+    }
+    goals.push(goal);
+  }
+  return goals;
+}
