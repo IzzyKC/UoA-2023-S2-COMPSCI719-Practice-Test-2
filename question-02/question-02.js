f5763900a9816abb5b5b144ff9d510510c0cf24d
changwanling <wcha305@aucklanduni.ac.nz>
2023-10-05 11:55:15 +1300

    question 02

--- question-02/question-02.js
+++ question-02/question-02.js
@@ -31,6 +31,43 @@ const upload = multer({
 // Make the "public" folder available statically
 app.use(express.static(path.join(__dirname, "public")));
 
+app.get("/", function(req, res) {
+  const fileNames = fs.readdirSync("public/files");
+  //res.locals.fileNames = fileNames;
+  const pdfs = fileNames.filter(function (fileName) {
+    const extension = fileName.toLowerCase().substring(fileName.lastIndexOf("."));
+    return ".pdf" == (extension);
+  });
+
+  console.log(pdfs);
+
+  const pptxs = fileNames.filter(function (fileName) {
+    const extension = fileName.toLowerCase().substring(fileName.lastIndexOf("."));
+    return ".pptx" == (extension);
+  });
+
+  console.log(pptxs);
+
+  res.locals.pdfs = pdfs;
+  res.locals.pptxs = pptxs;
+
+  res.render("pgcertfiles");
+});
+
+app.post("/uploadFile", upload.single("file"), async function (req, res) {
+
+  const fileInfo = req.file;
+
+  // Move the image into the images folder
+  const oldFileName = fileInfo.path;
+  const newFileName = `./public/files/${fileInfo.originalname}`;
+  fs.renameSync(oldFileName, newFileName);
+
+  // Redirect back to the homepage.
+  res.redirect("/");
+
+});
+
 // Start the server running. Once the server is running, the given function will be called, which will
 // log a simple message to the server console. Any console.log() statements in your node.js code
 // can be seen in the terminal window used to run the server.
